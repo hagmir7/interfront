05acd1166c37dcfa68dd8ef2519ee6f3bd3a62ae
Agmir Hassane <hagmir6@gmail.com>
2025-03-21 18:20:55 +0000

    Add Blog

--- src/app/(pages)/aprops/page.jsx
+++ src/app/(pages)/aprops/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function about() {
-  throw new Error("This is an error");
+  // throw new Error("This is an error");
   return (
     <div>about</div>
   )
