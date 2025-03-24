41e0ac79a9d6ce07e1d855eb79dfa186453b119a
Agmir Hassane <hagmir6@gmail.com>
2025-03-24 08:33:06 +0000

    Start in home blog section

--- src/app/(pages)/aprops/page.jsx
+++ src/app/(pages)/aprops/page.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export default function about() {
+  throw new Error('Error in about');
   return (
     <div>about</div>
   )
--- src/components/blog-section.jsx
+++ src/components/blog-section.jsx
@@ -1,33 +1,13 @@
 import React from 'react';
 
-const BlogSection = () => {
-  // Blog post data
-  const blogPosts = [
-    {
-      id: 1,
-      title: "Aménagement de cuisine au Marco : Transformer votre espace culinaire",
-      shortTitle: "Aménagement de cuisine au Marco : Transformer votr...",
-      excerpt: "L'aménagement de cuisine au Marco représente bien plus qu'un simple agencement d...",
-      image: "https://intercocina.com/storage/public/01JNZYNBTGWSYNKC7CBKDQB617.png",
-      url: "https://intercocina.com/blogs/amenagement-de-cuisine-au-marco-transformer-votre-espace-culinaire"
-    },
-    {
-      id: 2,
-      title: "Parquet Stratifié : Le Revêtement de Sol Idéal au Maroc",
-      shortTitle: "Parquet Stratifié : Le Revêtement de Sol Idéal au...",
-      excerpt: "le parquet stratifié est le revêtement de sol idéal au Maroc. Qualité, prix comp...",
-      image: "https://intercocina.com/storage/public/01JMVVY76PG7RM6DB96Q38MDPK.webp",
-      url: "https://intercocina.com/blogs/parquet-stratifie-le-revetement-de-sol-ideal-au-maroc"
-    },
-    {
-      id: 3,
-      title: "Le Meilleur Cuisiniste au Maroc : Pour une Cuisine Moderne",
-      shortTitle: "Le Meilleur Cuisiniste au Maroc : Pour une Cuisine...",
-      excerpt: "Intercocina, le meilleur cuisiniste au Maroc, spécialisé dans la fabrication de...",
-      image: "https://intercocina.com/storage/public/01JKWH34SD1SMTJ6K2E33EMSXC.webp",
-      url: "https://intercocina.com/blogs/le-meilleur-cuisiniste-au-maroc-pour-une-cuisine-moderne"
-    }
-  ];
+const BlogSection = async () => {
+  
+  const respones = await fetch('http://interapi.facepy.com/api/posts/home');
+  if(!respones.ok) {
+    throw new Error('Something went wrong');
+  }
+  
+  const articles = respones.json();
 
   return (
     <section className="py-12 overflow-x-hidden">
@@ -41,7 +21,7 @@ const BlogSection = () => {
       </div>
       <div className="px-4 max-w-7xl mx-auto">
         <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
-          {blogPosts.map(post => (
+          {articles.data.map(post => (
             <div key={post.id} className="col-span-full md:col-span-1">
               <a className="block" href={post.url} target="_blank" rel="noopener noreferrer">
                 <div className="overflow-hidden w-full bg-white rounded-lg border">
