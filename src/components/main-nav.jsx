7562fb98ec74643758d33de20deb90692569fdb6
Agmir Hassane <hagmir6@gmail.com>
2025-04-03 08:57:07 +0000

    add authenitcation

--- src/components/main-nav.jsx
+++ src/components/main-nav.jsx
@@ -16,7 +16,7 @@ import { ProductsMenu } from "./products-menu";
 import TopNav from "./top-nav";
 import CartCounter from "./CartCounter";
 import CLink from "./CLink";
-import { getCurrentUser } from "@/services/auth";
+// import { getCurrentUser } from "@/services/auth";
 
 const MainNav = () => {
     return (
@@ -110,7 +110,7 @@ const MainNav = () => {
                     </CLink>
 
                     <CLink href="/profile" className="hover:text-gray-600 p-2 rounded-full bg-[#efeeeb]">
-                        {getCurrentUser() }
+                        {/* {getCurrentUser() } */}
                         <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
                             <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M15 7.5a3 3 0 1 1-6 0a3 3 0 0 1 6 0m4.5 13c-.475-9.333-14.525-9.333-15 0"></path>
                         </svg>
