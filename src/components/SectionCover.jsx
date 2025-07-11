94e7008cd7c2af9e347945f4215b4e4724a1704e
Agmir Hassane <hagmir6@gmail.com>
2025-07-11 11:02:35 +0100

    Add background images

--- src/components/SectionCover.jsx
+++ src/components/SectionCover.jsx
@@ -121,19 +121,26 @@ const SectionConver = () => {
           </div>
         </div>
 
-        {/* Decorative Element */}
-        <div className='absolute bottom-0 left-0 right-0'>
-          <svg
-            viewBox='0 0 1440 120'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <path
-              d='M0 120L60 105C120 90 240 60 360 45C480 30 600 30 720 37.5C840 45 960 60 1080 67.5C1200 75 1320 75 1380 75L1440 75V120H1380C1320 120 1200 120 1080 120C960 120 840 120 720 120C600 120 480 120 360 120C240 120 120 120 60 120H0Z'
-              fill='white'
-            />
-          </svg>
-        </div>
+<div
+  className="absolute bottom-0 left-0 right-0 h-[129px]"
+  style={{
+    backgroundImage: "url('/imgs/bg-inter1.png')",
+    backgroundSize: '100px',
+    backgroundRepeat: 'repeat',
+    backgroundPosition: 'top',
+    transform: 'scaleY(-1)',
+    opacity: '28%',
+    bottom: '-25px',
+    WebkitMaskRepeat: 'no-repeat',
+    maskRepeat: 'no-repeat',
+    maskSize: 'cover',
+  }}
+></div>
+
+
+
+
+
       </section>
     )
 };
