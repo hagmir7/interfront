599ad24ee15679cfed32168d28d103b95944bf2b
Agmir Hassane <hagmir6@gmail.com>
2026-02-16 11:48:21 +0000

    modified:   src/components/ui/main-section-grid.jsx

--- src/components/ui/main-section-grid.jsx
+++ src/components/ui/main-section-grid.jsx
@@ -14,27 +14,29 @@ import ColorCards from './ColorCards';
 const MainSectionGrid = () => {
   const [activeSlide, setActiveSlide] = useState(0);
 
-    const [data, setData] = useState([]);
-  
+  const [data, setData] = useState([]);
+
   const getData = async () => {
     try {
       const response = await api.get('covers');
       setData(response.data);
       console.log(response.data);
-      
+
     } catch (error) {
 
     }
   }
 
-    const IMAGE_BASE_URL = isProduction
-      ? 'https://https://interapi.facepy.com//storage/'
-      : 'http://localhost:8000/storage/';
-  
-  
-    useEffect(() =>{
-      getData()
-    }, [])
+  const isProduction = process.env.NODE_ENV === 'production';
+
+  const IMAGE_BASE_URL = isProduction
+    ? 'https://https://interapi.facepy.com//storage/'
+    : 'http://localhost:8000/storage/';
+
+
+  useEffect(() => {
+    getData()
+  }, [])
 
 
   const sideProductsData = [
@@ -70,62 +72,62 @@ const MainSectionGrid = () => {
             onSlideChange={(swiper) => setActiveSlide(swiper.activeIndex)}
             className='hero-swiper'
           >
-           {data.map((item, index) => (
-  <SwiperSlide key={index}>
-    <div className='grid grid-cols-1 md:grid-cols-2 items-center py-3 md:py-9 px-3 md:p-8'>
-      <div className='order-2 sm:order-1 space-y-4'>
-        <div className='flex items-center gap-4'>
-          <span className='text-lg md:text-2xl sm:text-4xl font-bold text-red-600'>
-            {item.price}
-          </span>
-          <span className='text-sm text-gray-700 uppercase tracking-wider'>
-            À partir
-            <br />
-            De
-          </span>
-        </div>
+            {data.map((item, index) => (
+              <SwiperSlide key={index}>
+                <div className='grid grid-cols-1 md:grid-cols-2 items-center py-3 md:py-9 px-3 md:p-8'>
+                  <div className='order-2 sm:order-1 space-y-4'>
+                    <div className='flex items-center gap-4'>
+                      <span className='text-lg md:text-2xl sm:text-4xl font-bold text-red-600'>
+                        {item.price}
+                      </span>
+                      <span className='text-sm text-gray-700 uppercase tracking-wider'>
+                        À partir
+                        <br />
+                        De
+                      </span>
+                    </div>
 
-        <h2 className='text-lg sm:text-3xl font-semibold text-gray-900 hover:text-red-600 transition mb-2'>
-          {
-            item?.url
-              ? <CLink href={item.url}>{item.title}</CLink>
-              : <p>{item.title}</p>
-          }
-        </h2>
+                    <h2 className='text-lg sm:text-3xl font-semibold text-gray-900 hover:text-red-600 transition mb-2'>
+                      {
+                        item?.url
+                          ? <CLink href={item.url}>{item.title}</CLink>
+                          : <p>{item.title}</p>
+                      }
+                    </h2>
 
-        <ColorCards colors={item.colors} />
+                    <ColorCards colors={item.colors} />
 
-        <p className='text-gray-600 mb-7 text-sm md:text-base'>
-          {item.description}
-        </p>
+                    <p className='text-gray-600 mb-7 text-sm md:text-base'>
+                      {item.description}
+                    </p>
 
-        <div className='flex justify-end md:justify-start w-full'>
-          <CLink
-            href={item.url || '#'}
-            className='rounded-lg flex gap-2 text-white bg-[#da3036] hover:bg-red-600 py-1.5 md:py-3 px-3 md:px-5 text-sm md:text-[17px] hover:text-white'
-          >
-            <span> Voir Plus</span>
-            <ArrowRight className='h-5 md:h-6' />
-          </CLink>
-        </div>
-      </div>
-      <div className='order-1 sm:order-2 mb-4 sm:mb-0 relative'>
-        
-        <span className="absolute top-4 right-4  text-white text-xs md:text-sm font-bold px-4 py-1.5 animate-pulse">
-          <Image width={50} height={50} alt='New' src={'/icons/new-badge-3-256.png'} />
-        </span>
+                    <div className='flex justify-end md:justify-start w-full'>
+                      <CLink
+                        href={item.url || '#'}
+                        className='rounded-lg flex gap-2 text-white bg-[#da3036] hover:bg-red-600 py-1.5 md:py-3 px-3 md:px-5 text-sm md:text-[17px] hover:text-white'
+                      >
+                        <span> Voir Plus</span>
+                        <ArrowRight className='h-5 md:h-6' />
+                      </CLink>
+                    </div>
+                  </div>
+                  <div className='order-1 sm:order-2 mb-4 sm:mb-0 relative'>
 
-        <Image
-          src={IMAGE_BASE_URL + item.image}
-          alt={item.title}
-          width={1000}
-          height={1000}
-          className='w-full h-auto object-contain max-h-[300px] md:max-h-[400px]'
-        />
-      </div>
-    </div>
-  </SwiperSlide>
-))}
+                    <span className="absolute top-4 right-4  text-white text-xs md:text-sm font-bold px-4 py-1.5 animate-pulse">
+                      <Image width={50} height={50} alt='New' src={'/icons/new-badge-3-256.png'} />
+                    </span>
+
+                    <Image
+                      src={IMAGE_BASE_URL + item.image}
+                      alt={item.title}
+                      width={1000}
+                      height={1000}
+                      className='w-full h-auto object-contain max-h-[300px] md:max-h-[400px]'
+                    />
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
 
           </Swiper>
         </div>
@@ -155,14 +157,14 @@ const MainSectionGrid = () => {
               </div>
               <div className='ml-4 flex-shrink-0'>
                 <CLink href={product.link}>
-                <Image
-                  src={product.image}
-                  alt={product.title}
-                  // fill
-                  width={200}
-                  height={200}
-                  className='w-32 h-32 object-contain'
-                />
+                  <Image
+                    src={product.image}
+                    alt={product.title}
+                    // fill
+                    width={200}
+                    height={200}
+                    className='w-32 h-32 object-contain'
+                  />
                 </CLink>
               </div>
             </div>
