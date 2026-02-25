69880d72f89abce66ee34a0f646d56741cb9a46a
Agmir Hassane <hagmir6@gmail.com>
2026-02-25 08:18:24 +0000

    Update the order details

--- src/app/(pages)/user/login/page.jsx
+++ src/app/(pages)/user/login/page.jsx
@@ -6,7 +6,7 @@ export default function LoginPage() {
 
 
     return (
-        <div className="p-10 max-w-5xl mx-auto">
+        <div className="p-4 md:p-10 max-w-5xl mx-auto">
             <Suspense fallback={<div>Chargement...</div>}>
                 <LoginForm />
             </Suspense>
--- src/app/(pages)/user/register/page.jsx
+++ src/app/(pages)/user/register/page.jsx
@@ -3,7 +3,7 @@ import React, { Suspense } from 'react'
 
 function page() {
   return (
-    <div className="p-10 max-w-5xl mx-auto">
+    <div className="p-4 md:p-10 max-w-5xl mx-auto">
       <Suspense fallback={<div>Chargement...</div>}>
         <RegisterForm isRegister={true} />
       </Suspense>
