2bb3f9caebf26cee34725934bffb952225173b2a
Agmir Hassane <hagmir6@gmail.com>
2026-06-17 15:20:32 +0100

    modified:   src/app/page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -22,6 +22,10 @@ export const metadata = {
   title: "Fabricant de meubles de cuisine de lux. - Intercocina",
   description:
     "Intercocina, la fabrication de meubles de cuisine, meubles TV, placards et armoires, meubles de salle de bain, ainsi que de parquets, au Maroc.",
+
+  alternates: {
+    canonical: `/`,
+  },
 };
 
 export default function Home() {
